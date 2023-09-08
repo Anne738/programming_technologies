4571be166467dec0e4c68f9711d658baf65d5d4b
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-09-08 21:52:50 +0300

    1

--- 2.py
+++ 2.py
@@ -1,13 +1,14 @@
 
-a = int(input("input num -> "))
+a = int(input("input first num -> "))
+b = int(input("input second num -> "))
 
-def num_minus (num):
-    print(num)
-    if num > 0:
-        return num_minus(num-1)
-    if num == 0:
+def num_minus (num1, num2):
+    print(num1)
+    if num1 > num2:
+        return num_minus(num1-1)
+    if num1 == 0:
         return 0  
-    if num < 0:
+    if num1 < 0:
         print("Число повинно бути натуральним")
         return 0
 
