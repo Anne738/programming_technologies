baace1675035feccd317b5dda0e22cd7b0b67a66
Anne738 <94481296+Anne738@users.noreply.github.com>
2023-10-01 22:42:36 +0300

    done

---
+++ lab2/prog.py
@@ -0,0 +1,37 @@
+
+
+class Order:
+    def __init__(self, item, quantity):
+        self.item = item
+        self.quantity = quantity
+
+class OrderStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, i, q):
+        order = Order(i, q)
+        try:
+            if order.quantity > 0:
+                self.stack.append(order)
+            else:
+                #print("Помилка: Кількість товару повинна бути більше 0.")
+                return False
+        except TypeError:
+            print("Помилка: Невірний формат замовлення.")
+
+    def pop(self):
+        try:
+            order = self.stack.pop()
+            #print(f"Замовлення на {order.item} ({order.quantity} одиниць) виконано.")
+            return -1
+        except IndexError:
+            print("Стек замовлень порожній.")
+
+    def view(self):
+        #print("Список замовлень:")
+        for order in self.stack:
+            print(f"- {order.item} ({order.quantity} одиниць)")
+
+
+
